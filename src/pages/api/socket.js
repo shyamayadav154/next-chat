09242f2a585670a37cb99632c525a27835ea636e
shyam <shyamayadav154@gmail.com>
2023-03-30 20:36:25 +0530

    cors enabled

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -1,6 +1,7 @@
 import { addUser, getUser, removeUser } from "@/utils/user";
 import nc from "next-connect";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const handler = nc({
   onError: (err, req, res, _next) => {
@@ -10,57 +11,59 @@ const handler = nc({
   onNoMatch: (req, res) => {
     res.status(404).end("Page is not found");
   },
-}).get((req, res) => {
-  if (res.socket.server.io) {
-    console.log("Already set up");
-    res.end();
-    return;
-  }
+})
+  .use(cors())
+  .get((req, res) => {
+    if (res.socket.server.io) {
+      console.log("Already set up");
+      res.end();
+      return;
+    }
 
-  const io = new Server(res.socket.server);
-  res.socket.server.io = io;
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
 
-  io.on("connection", (socket) => {
-    socket.on("join-room", ({ name, room }, callback) => {
-      const { user, error } = addUser({ id: socket.id, name, room });
+    io.on("connection", (socket) => {
+      socket.on("join-room", ({ name, room }, callback) => {
+        const { user, error } = addUser({ id: socket.id, name, room });
 
-      if (error) {
-        return callback(error);
-      }
-      socket.join(user.room);
+        if (error) {
+          return callback(error);
+        }
+        socket.join(user.room);
 
-      socket.emit("message", {
-        user: name,
-        message: `${name} has joined the room for all`,
+        socket.emit("message", {
+          user: name,
+          message: `${name} has joined the room for all`,
+        });
+
+        socket.broadcast.to(user.room).emit("message", {
+          user: name,
+          message: `${user.name} has joined the room from broadcast`,
+        });
+
+        callback();
       });
 
-      socket.broadcast.to(user.room).emit("message", {
-        user: name,
-        message: `${user.name} has joined the room from broadcast`,
+      socket.on("send-message", (obj) => {
+        const user = getUser(socket.id);
+        console.log("Received message", obj);
+        io.to(user.room).emit("receive-message", obj);
       });
 
-      callback();
-    });
-
-    socket.on("send-message", (obj) => {
-        const  user = getUser(socket.id)
-      console.log("Received message", obj);
-      io.to(user.room).emit("receive-message", obj);
-    });
-
-    socket.on("disconnect", () => {
-      const  user  = removeUser(socket.id);
-      if(user){
-        io.to(user.room).emit("message", {
+      socket.on("disconnect", () => {
+        const user = removeUser(socket.id);
+        if (user) {
+          io.to(user.room).emit("message", {
             message: `${user.name} has left the room`,
-            });
-      }
-      console.log("User has left");
+          });
+        }
+        console.log("User has left");
+      });
     });
-  });
 
-  console.log("Setting up socket");
-  res.end();
-});
+    console.log("Setting up socket");
+    res.end();
+  });
 
 export default handler;
