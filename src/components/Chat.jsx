dbc5252b8d840161caacbc3475f4ac6043c2b477
shyam <shyamayadav154@gmail.com>
2023-03-31 12:52:51 +0530

    autofocus on input and placeholder added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -121,8 +121,10 @@ function ChatBox({ sendMessage, allMessages }) {
           <input
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             type="text"
+            placeholder="Type your message"
             value={input}
             onChange={onChangeHandler}
+            autoFocus
           />
           <button className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Send
